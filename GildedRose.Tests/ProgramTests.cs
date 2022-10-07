e7e8fe1a0486b4d66bee75b52f8a4188d62632ac
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-10-07 12:48:49 +0200

    test method, no body

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -7,4 +7,51 @@ public class ProgramTests
     {
         true.Should().BeTrue();
     }
+
+    [Fact]
+    public void Test_negative_quality()
+    {
+        
+    }
+
+    [Fact]
+    public void Test_quality_over_50()
+    {
+        
+    }
+    [Fact]
+    public void Test_normal_item_degrades_before_sell()
+    {
+        
+    }
+
+    [Fact]
+    public void Test_normal_item_degrades_twice_fast_after_sell()
+    {
+        
+    }
+
+    [Fact]
+    public void Test_brie_rise_quality_always()
+    {
+        
+    }
+
+    [Fact]
+    public void Test_backstage_increase_10_days_before_sell()
+    {
+        
+    }
+    [Fact]
+    public void Test_backstage_increase_5_days_before_sell()
+    {
+        
+    }
+
+
+ [Fact]
+    public void Test_backstage_after_sell_worth_0()
+    {
+        
+    }
 }
