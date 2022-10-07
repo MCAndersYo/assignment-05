ee2ba14962eac2d6c12c3d4dd972cf91a535ba10
Anders Arvesen <aarv@itu.dk>
2022-10-07 13:30:06 +0200

    one test fails, since items can be added with quality over 50 in current implementation

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 namespace GildedRose.Tests;
+using GildedRose;
 
 public class ProgramTests
 {
@@ -9,20 +10,84 @@ public class ProgramTests
     }
 
     [Fact]
-    public void Test_negative_quality()
+    public void Test_never_negative_quality()
     {
+        //Arrange
+        var program = new Program();
+        ;
+        program.Items = new List<Item>(){new Item{ Name = "+5 Dexterity Vest", SellIn = 10, Quality = 0 }};
         
+        //Act
+        program.UpdateQuality();
+        
+        //Assert
+        program.Items.First().SellIn.Should().Be(9);
+        program.Items.First().Quality.Should().Be(0);
+
+    }
+
+    public void Test_never_quality_when_added_item_has_negative_quality()
+    {
+        //Arrange
+        var program = new Program();
+        ;
+        program.Items = new List<Item>(){new Item{ Name = "+5 Dexterity Vest", SellIn = 10, Quality = -1 }};
+        
+        //Act
+        program.UpdateQuality();
+        
+        //Assert
+        program.Items.First().SellIn.Should().Be(9);
+        program.Items.First().Quality.Should().Be(0);
+
     }
 
     [Fact]
-    public void Test_quality_over_50()
+    public void Test_quality_never_over_50()
     {
+        //Arrange
+        var program = new Program();
+        ;
+        program.Items = new List<Item>(){new Item{ Name = "Aged Brie", SellIn = 2, Quality = 50}};
         
+        //Act
+        program.UpdateQuality();
+        
+        //Assert
+        program.Items.First().SellIn.Should().Be(1);
+        program.Items.First().Quality.Should().Be(50);
     }
+
+    [Fact]
+    public void Test_quality_never_over_50_when_added_item_added_has_quality_over_50()
+    {
+        //Arrange
+        var program = new Program();
+        ;
+        program.Items = new List<Item>(){new Item{ Name = "Aged Brie", SellIn = 2, Quality = 80}};
+        
+        //Act
+        program.UpdateQuality();
+        
+        //Assert
+        program.Items.First().SellIn.Should().Be(1);
+        program.Items.First().Quality.Should().Be(50);
+    }
+
     [Fact]
     public void Test_normal_item_degrades_before_sell()
     {
+        //Arrange
+        var program = new Program();
+        ;
+        program.Items = new List<Item>(){new Item{ Name = "+5 Dexterity Vest", SellIn = 10, Quality = 10}};
         
+        //Act
+        program.UpdateQuality();
+        
+        //Assert
+        program.Items.First().SellIn.Should().Be(9);
+        program.Items.First().Quality.Should().Be(9);
     }
 
     [Fact]
