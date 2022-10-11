18fb2a3667e0f40db24c60e5073fe3e6c3cdcc15
lufr@itu.dk <lufr@itu.dk>
2022-10-11 12:55:26 +0200

    added extra conjured test and deleted redundant test

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -258,7 +258,6 @@ public class ProgramTests
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
 				new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
             });
     }
